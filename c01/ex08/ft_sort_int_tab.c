d5b8d0c4000080224a8867f149bac541f150af26
RightHennessy <myheaven_b@naver.com>
2022-10-04 17:29:20 +0900

    add ex08

---
+++ c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yerchoi <yerchoi@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 16:29:06 by yerchoi           #+#    #+#             */
+/*   Updated: 2022/10/03 17:35:42 by yerchoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	swap(int *a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size - i)
+		{
+			if (tab[j] > tab[j + 1])
+				swap(&tab[j], &tab[j + 1]);
+			j++;
+		}
+		i++;
+	}
+}
